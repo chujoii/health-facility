11e2c098d7f7c711ac3aa699420012556c1c9f4c
Roman V. Prikhodchenko <chujoii@gmail.com>
2020-03-20 21:41:27 +0300

    Use components

--- src/client/registry.js
+++ src/client/registry.js
@@ -1,7 +1,37 @@
-var doc_time_table = new Vue({
+Vue.component("tab-dt", {
+	props: ['title', 'doctors'],
+	data: function () {
+		return {
+			picked: ''
+		}
+	},
+	template: `
+		<div id="tab-dt"> \
+			{{ title }} \
+			Выберите врача и время приёма: \
+			<table> \
+				<tr> \
+					<th>врач</th> \
+					<th colspan="4">время</th> <!-- fixme: colspan --> \
+				</tr> \
+				<tr v-for="(doc, id_x) in doctors"> \
+					<td> \
+						{{doc.name}} \
+					</td> \
+					<td v-for="(tim, id_y) in doc.timetable"> \
+						<input type="radio" v-model="picked" :value="'' + id_x + ':' + id_y" /> \
+						<label for=doctors[id_x][id_y]>{{tim}}</label> \
+					</td> \
+				</tr> \
+			</table> \
+			Выбрано: {{ picked }} <br/> \
+		</div> \
+`
+});
+
+new Vue({
 	el: '#doc_time',
-	data: {
-		doctors: array_doctor_time,
-		picked: ''
+	methods: {
+		get_doc_time: function () {return array_doctor_time;}
 	}
 });
