df38f59e19c2688526cbf53e8a0c580bcdd6ae92
Roman V. Prikhodchenko <chujoii@gmail.com>
2020-03-26 23:10:42 +0300

    Unregister ticket server script

---
+++ src/server/unregister_ticket.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Danger: do not use shell scripts in real server!
+# Danger: script delete files from /tmp directory!
+
+echo Content-type: text/html
+echo
+
+echo '<!DOCTYPE html>'
+echo '<html>'
+echo '<head>'
+echo '<title>test</title>'
+echo '<meta http-equiv=content-type content="text/html; charset=utf-8">'
+echo '</head>'
+echo '<body>'
+
+echo '<h1>Query string POST:</h1>'
+echo '<pre>'
+
+# save POST variables
+[ -z "$QUERY_STRING_POST"  -a "$REQUEST_METHOD" = "POST" -a ! -z "$CONTENT_LENGTH" ] && read -n $CONTENT_LENGTH QUERY_STRING_POST
+echo $QUERY_STRING_POST
+
+FILE_NAME=`echo $QUERY_STRING_POST | perl -ne 'while(/(?<=id":")[0-9.]+/g){print "$&\n";}'`
+rm "/tmp/$FILE_NAME.txt"
+
+echo '</pre>'
+
+echo '</body>'
+echo '</html>'
